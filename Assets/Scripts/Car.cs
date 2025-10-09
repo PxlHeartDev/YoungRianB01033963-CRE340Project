8d2c8b19a924d6ddb53f17619721202335e0c2b4
pxlheartdev <rianyoungni@outlook.com>
2025-10-09 16:00:17 +0100

    IT ACTUALLY WORKS!!!

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -4,61 +4,72 @@ using UnityEngine;
 public class Car : MonoBehaviour
 {
     [SerializeField] protected Rigidbody rb;
+    [SerializeField] private GameObject wheelPrefab;
+    [SerializeField] private Vector2 wheelDistance = new Vector2(2, 2);
 
-    public float gasStrength = 1.0f;
-
-    private float steerDegrees = 90.0f;
-
-    public GameObject wheelPrefab;
-    public Vector2 wheelDistance = new Vector2(2, 2);
+    public bool debug = false;
 
     Wheel[] wheels = new Wheel[4];
-    float rpm = 0;
 
-    float maxSuspensionLength = 1.3f;
+    public float gasStrength = 40.0f;
+    private float steerDegrees = 30.0f;
+    private float rpm = 0.0f;
 
-    private void Awake()
+    void Awake()
     {
         for (int i = 0; i < 4; i++)
         {
             wheels[i] = new Wheel();
             wheels[i].rbCar = rb;
-            wheels[i].position = transform.position;
-            wheels[i].restPosition = wheels[i].position;
+            wheels[i].wheelTransform = new GameObject();
+            wheels[i].wheelTransform.transform.position = transform.position;
+            wheels[i].debug = debug;
 
-            wheels[i].transform = transform;
+            switch (i) {
+                case (0):
+                    wheels[0].wheelTransform.transform.position = transform.position + transform.right * wheelDistance.x + transform.forward * wheelDistance.y;//front right
+                    break;
+                case (1):
+                    wheels[1].wheelTransform.transform.position = transform.position + transform.right * -wheelDistance.x + transform.forward * wheelDistance.y;//front left
+                    break;
+                case (2):
+                    wheels[2].wheelTransform.transform.position = transform.position + transform.right * wheelDistance.x + transform.forward * -wheelDistance.y;//back right
+                    break;
+                case (3):
+                    wheels[3].wheelTransform.transform.position = transform.position + transform.right * -wheelDistance.x + transform.forward * -wheelDistance.y;//back left
+                    break;
+            }
+            wheels[i].restPosition = wheels[i].wheelTransform.transform.position;
 
-            if (i == 0 || i == 1) wheels[i].front = true;
-            wheels[i].prefab = Instantiate(wheelPrefab, wheels[i].position, Quaternion.identity);
+            if (i == 0 || i == 2) wheels[i].front = true;
+            wheels[i].prefab = Instantiate(wheelPrefab, wheels[i].wheelTransform.transform.position, Quaternion.identity, transform);
             wheels[i].prefab.transform.eulerAngles = new Vector3(0.0f, 90.0f, 0.0f);
+
+            wheels[i].PostInitialize();
         }
     }
 
     void FixedUpdate()
     {
-        wheels[0].position = transform.right * wheelDistance.x + transform.forward * wheelDistance.y; //front right
-        wheels[1].position = transform.right * -wheelDistance.x + transform.forward * wheelDistance.y; //front left
-        wheels[2].position = transform.right * wheelDistance.x + transform.forward * -wheelDistance.y; //back right
-        wheels[3].position = transform.right * -wheelDistance.x + transform.forward * -wheelDistance.y; //back left
+        wheels[0].wheelTransform.transform.position = transform.position + transform.right * wheelDistance.x + transform.forward * wheelDistance.y; //front right
+        wheels[1].wheelTransform.transform.position = transform.position + transform.right * -wheelDistance.x + transform.forward * wheelDistance.y; //front left
+        wheels[2].wheelTransform.transform.position = transform.position + transform.right * wheelDistance.x + transform.forward * -wheelDistance.y; //back right
+        wheels[3].wheelTransform.transform.position = transform.position + transform.right * -wheelDistance.x + transform.forward * -wheelDistance.y; //back left
+
+
+        rpm -= ((rpm * Mathf.PI) + -transform.InverseTransformDirection(rb.linearVelocity).z) * Time.deltaTime * 0.1f;
 
         for (int i = 0; i < 4; i++)
         {
-            wheels[i].Update();
-
-            rpm += ((rpm * Mathf.PI) + -transform.InverseTransformDirection(rb.linearVelocity).z) * Time.deltaTime * 0.1f;
-
-            wheels[i].isOnGround = wheels[i].ShootRay();
-            RaycastHit hit = wheels[i].rayHit;
-            if (hit.collider != null)
+            wheels[i].wheelTransform.transform.rotation = transform.rotation;
+            if (debug)
             {
-                wheels[i].prefab.transform.position = hit.point;
-
-                wheels[i].prefab.transform.GetChild(0).Rotate(0, -rpm, 0, Space.Self);
+                Debug.DrawRay(wheels[i].wheelTransform.transform.position, wheels[i].wheelTransform.transform.forward, Color.blue);
+                Debug.DrawRay(wheels[i].wheelTransform.transform.position, wheels[i].wheelTransform.transform.right, Color.red);
+                Debug.DrawRay(wheels[i].wheelTransform.transform.position, wheels[i].wheelTransform.transform.up, Color.green);
             }
-            else
-            {
-                wheels[i].prefab.transform.position = transform.position + wheels[i].position - transform.up * (maxSuspensionLength);
-            }
+            wheels[i].rpm = rpm;
+            wheels[i].Update(transform);
         }
     }
 
@@ -66,7 +77,7 @@ public class Car : MonoBehaviour
     {
         for (int i = 0; i < 4; i++)
         {
-            rpm -= 1.0f * Time.deltaTime;
+            rpm += 1.0f * Time.deltaTime;
             if (wheels[i].isOnGround) 
             {
                 wheels[i].ApplyGas(gasStrength);
@@ -110,71 +121,72 @@ public class Car : MonoBehaviour
 
 class Wheel
 {
-    public Transform transform;
-    public Vector3 position;
+    public GameObject wheelTransform;
     public Vector3 restPosition;
     public GameObject prefab;
     public RaycastHit rayHit;
     public Rigidbody rbCar;
 
-    public bool isOnGround;
-
     public bool front;
 
-    float maxSuspensionLength = 1.3f;
-    float suspensionMultiplier = 50.0f;
-    float suspensionDamping = 5.0f;
-    float maxTraction = 240.0f;
-    float traction = 120.0f;
+    public bool debug = false;
 
-    [SerializeField] private float suspensionStrength = 100.0f;
-    [SerializeField] private float suspensionDampening = 15.0f;
-    [SerializeField] private float suspensionLength = 1.0f;
+    private readonly float maxTraction = 240.0f;
+    private readonly float frictionCoefficient = 1.1f;
+    private readonly float wheelGrip = 20.0f;
 
-    Vector3 suspensionForce;
-    Vector3 tractionForce;
+    private readonly float maxSuspensionLength = 2.2f;
+    private readonly float suspensionStrength = 200.0f;
+    private readonly float suspensionDampening = 20.0f;
+    private readonly float suspensionRestLength = 1.5f;
 
-    float rpm = 0.0f;
+    public bool isOnGround;
 
-    public void Update()
+    public float rpm = 0.0f;
+    private float currentSteer = 0.0f;
+
+    private float defaultPrefabRotation = 0.0f;
+
+    public void PostInitialize()
     {
-        transform.position = position;
-        if (isOnGround)
-        {
-            ApplyWheelForce(transform.up * CalculateSuspension());
-
-            //float vel = Vector3.Dot(rbCar.transform.up, rbCar.GetPointVelocity(position));
-            //suspensionForce = Vector3.ClampMagnitude(rbCar.transform.up * ((Mathf.Clamp(maxSuspensionLength - rayHit.distance, 0, 3) * suspensionMultiplier) - (vel * suspensionDamping)), 100.0f);
-            //ApplyWheelForce(suspensionForce);
-            //Debug.Log(suspensionForce);
-        }
-        if (position != null)
-        {
-            prefab.transform.position = position;
-        }
+        defaultPrefabRotation = prefab.transform.localEulerAngles.y;
     }
 
-    private float CalculateSuspension()
+    public void Update(Transform tf)
     {
-        Vector3 springAxis = transform.up;
-        Vector3 wheelWorldVelocity = rbCar.GetPointVelocity(transform.position);
-        float offset = suspensionLength - rayHit.distance;
+        isOnGround = ShootRay();
+
+        wheelTransform.transform.Rotate(new Vector3(0.0f, currentSteer, 0.0f));
+
+        if (isOnGround)
+        {
+            ApplyWheelForce(CalculateSuspension());
+            ApplyWheelForce(CalculateFriction());
+            ApplyWheelForce(CalculateAntiSlip());
+        }
 
 
-        float vel = Vector3.Dot(springAxis, wheelWorldVelocity);
-        float force = (offset * suspensionStrength) - (vel * suspensionDampening);
 
-        return force;
+        if (rayHit.collider != null)
+        {
+            prefab.transform.position = rayHit.point;
+        }
+        else
+        {
+            prefab.transform.position = Vector3.Lerp(prefab.transform.position, (wheelTransform.transform.position - rbCar.transform.up * 0.3f) - rbCar.transform.up * (maxSuspensionLength), 0.2f);
+        }
+
+
+        prefab.transform.GetChild(0).Rotate(0, rpm, 0, Space.Self);
     }
 
     public void ApplyGas(float gasStrength)
     {
         if (front && isOnGround)
         {
-            tractionForce = Vector3.ClampMagnitude(traction * (prefab.transform.right * -prefab.transform.InverseTransformDirection(rbCar.linearVelocity).x + prefab.transform.forward * ((rpm * Mathf.PI) + -prefab.transform.InverseTransformDirection(rbCar.linearVelocity).z)), maxTraction);
-            tractionForce = Quaternion.AngleAxis(prefab.transform.eulerAngles.y, Vector3.up) * tractionForce;
+            Vector3 accelerationForce = Vector3.ClampMagnitude(gasStrength * wheelTransform.transform.right, maxTraction);
 
-            ApplyWheelForce(tractionForce);
+            ApplyWheelForce(accelerationForce);
         }
     }
 
@@ -182,7 +194,8 @@ class Wheel
     {
         if (front)
         {
-            prefab.transform.eulerAngles = new Vector3(prefab.transform.eulerAngles.x, prefab.transform.eulerAngles.y + degrees, prefab.transform.eulerAngles.z);
+            currentSteer = degrees;
+            prefab.transform.eulerAngles = new Vector3(prefab.transform.eulerAngles.x, defaultPrefabRotation + degrees, prefab.transform.eulerAngles.z);
         }
         else
         { 
@@ -190,14 +203,46 @@ class Wheel
         }
     }
 
-    private void ApplyWheelForce(Vector3 force)
-    {
-        rbCar.AddForceAtPosition(force, rayHit.point);
-    }
-
-
     public bool ShootRay()
     {
-        return Physics.Raycast(restPosition + rbCar.transform.up * 0.1f, -rbCar.transform.up, out rayHit, maxSuspensionLength);
+        if (debug) {
+            Debug.DrawRay(wheelTransform.transform.position - rbCar.transform.up * 0.3f, -rbCar.transform.up, Color.green);
+            Debug.DrawRay(rayHit.point, rayHit.normal, Color.purple);
+        }
+        return Physics.Raycast(wheelTransform.transform.position - rbCar.transform.up * 0.3f, -rbCar.transform.up, out rayHit, maxSuspensionLength);
+    }
+
+    private void ApplyWheelForce(Vector3 force)
+    {
+        rbCar.AddForceAtPosition(force, wheelTransform.transform.position);
+    }
+
+    private Vector3 CalculateSuspension()
+    {
+        Vector3 springAxis = wheelTransform.transform.up;
+        Vector3 wheelWorldVelocity = rbCar.GetPointVelocity(wheelTransform.transform.position);
+        float offset = suspensionRestLength - rayHit.distance;
+
+
+        float vel = Vector3.Dot(springAxis, wheelWorldVelocity);
+        float force = (offset * suspensionStrength) - (vel * suspensionDampening);
+
+        return wheelTransform.transform.up * force;
+    }
+
+    private Vector3 CalculateFriction()
+    {
+        Vector3 vel = rbCar.GetPointVelocity(wheelTransform.transform.position);
+
+        return -vel * frictionCoefficient;
+    }
+
+    private Vector3 CalculateAntiSlip()
+    {
+        float steerVelDot = Vector3.Dot(wheelTransform.transform.forward, rbCar.GetPointVelocity(wheelTransform.transform.position));
+
+        float negationForce = -steerVelDot * wheelGrip;
+
+        return wheelTransform.transform.forward * negationForce;
     }
 }
