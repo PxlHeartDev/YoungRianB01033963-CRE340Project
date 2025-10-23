06b191b8e5e3f4dbbdafc4854f8dd19f1d741c05
pxlheartdev <rianyoungni@outlook.com>
2025-10-23 12:38:20 +0100

    Added coin model and animation

--- Assets/Scripts/Entities/Coin.cs
+++ Assets/Scripts/Entities/Coin.cs
@@ -2,17 +2,33 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour, ICollectable
 {
+    [SerializeField] private GameObject model;
+    private Animation anim;
+
     public int scoreValue = 1;
 
-    // Update is called once per frame
-    void Update()
+    void Awake()
     {
-        
+        anim = model.GetComponent<Animation>();
+        //anim.Play();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.ToString());
+        if (other.gameObject.tag == "Player")
+        {
+            Collect(other.gameObject);
+        }
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        Collect(collision.collider.gameObject);
+        Debug.Log(collision.ToString()); 
+        if (collision.collider.gameObject.tag == "Player")
+        {
+            Collect(collision.collider.gameObject);
+        }
     }
 
     public void Collect(GameObject source)
