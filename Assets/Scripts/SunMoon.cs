3f07aa415ea0bc92e5fd20881d7aa84f675deb6a
pxlheartdev <rianyoungni@outlook.com>
2025-11-25 13:01:21 +0000

    Basic sun/moon/day/night cycle implementation

---
+++ Assets/Scripts/SunMoon.cs
@@ -0,0 +1,104 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class SunMoon : MonoBehaviour
+{
+    [SerializeField] new Light light;
+    [SerializeField] Material daySkybox;
+    [SerializeField] Material nightSkybox;
+    [SerializeField] private float dayLength;
+    [SerializeField] private float rotThreshold1 = 200.0f;
+    [SerializeField] private float rotThreshold2 = 220.0f;
+
+    private Vector3 startingRotation;
+    private Vector3 targetRotation;
+
+    public System.Action sunsetTrigger;
+    public System.Action sunriseTrigger;
+
+    private bool isNight;
+
+    private Dictionary<string, TimeOfDay> timesOfDay = new();
+
+    float rotCutPoint = 180.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SetRots();
+
+        timesOfDay.Add("Day", new TimeOfDay(
+            "Day",
+            Color.white,
+            5000,
+            2.0f));
+        timesOfDay.Add("Night", new TimeOfDay(
+            "Night",
+            new Color(0.588f, 0.588f, 1.0f),
+            20000,
+            0.5f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.rotation = Quaternion.Lerp(Quaternion.Euler(startingRotation), Quaternion.Euler(targetRotation), Time.time);
+
+        targetRotation.x += (360.0f/dayLength) * Time.deltaTime;
+
+        if (targetRotation.x > rotThreshold1)
+        {
+            light.intensity = Mathf.Lerp(light.intensity, 0.0f, (targetRotation.x - rotThreshold1)/(rotThreshold2 - rotThreshold1));
+        }
+
+        if (targetRotation.x > rotThreshold2)
+        {
+            if (isNight)
+                SetToDay();
+            else
+                SetToNight();
+            targetRotation.x = (rotThreshold1 - rotThreshold2) * 2.0f;
+        }
+    }
+
+    void SetRots()
+    {
+        startingRotation = transform.eulerAngles;
+        targetRotation = startingRotation;
+    }
+
+    void SetToDay()
+    {
+        isNight = false;
+        light.color = timesOfDay["Day"].colour;
+        light.colorTemperature = timesOfDay["Day"].kelvin;
+        light.intensity = timesOfDay["Day"].intensity;
+        RenderSettings.skybox = daySkybox;
+    }
+
+    void SetToNight()
+    {
+        isNight = true;
+        light.color = timesOfDay["Night"].colour;
+        light.colorTemperature = timesOfDay["Night"].kelvin;
+        light.intensity = timesOfDay["Night"].intensity;
+        RenderSettings.skybox = nightSkybox;
+    }
+}
+
+public class TimeOfDay
+{
+    public string name;
+    public Color colour;
+    public int kelvin;
+    public float intensity;
+
+    public TimeOfDay(string _name, Color _colour, int _kelvin, float _intensity)
+    {
+        name = _name;
+        colour = _colour;
+        kelvin = _kelvin;
+        intensity = _intensity;
+    }
+}
