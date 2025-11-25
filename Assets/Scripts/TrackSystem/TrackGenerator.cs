0e8ed29a821468d993d46c092566476923ec1398
pxlheartdev <rianyoungni@outlook.com>
2025-11-25 15:55:54 +0000

    Commented TrackGenerator.cs
    Also made some minor performance tweaks

--- Assets/Scripts/TrackSystem/TrackGenerator.cs
+++ Assets/Scripts/TrackSystem/TrackGenerator.cs
@@ -67,34 +67,42 @@ public class TrackGenerator : MonoBehaviour
     }
     public void CreateInitialPiece()
     {
+        // Instantiate the new track piece
         pieces.Add(new TrackPiece(trackScale));
+
+        // Assign all the parameters
         pieces[0].trackWidth = defaultTrackWidth;
         pieces[0].trackHeight = defaultTrackHeight;
         pieces[0].precision = precision;
         pieces[0].barrierWidth = defaultBarrierWidth;
         pieces[0].barrierHeight = defaultBarrierHeight;
         pieces[0].doBarriers = doBarriers;
-
+        
+        // Connect signals
         pieces[0].roadMeshPieceGenerated += RoadMeshPieceGenerated;
         pieces[0].roadSegmentDeleted += RoadSegmentDeleted;
 
         pieces[0].mountainMeshGenerated += MountainMeshPieceGenerated;
         pieces[0].mountainSegmentDeleted += MountainSegmentDeleted;
-
+        
+        // Initalize
         pieces[0].InitSegment(transform.position);
         GenerateInitialPieces();
     }
 
+    // Generate the next segment of the track
     private void GenerateNextSegment()
     {
         pieces[0].AddSegment(new Vector3(Random.Range(-3.0f, 3.0f), Random.Range(-1.0f, 1.0f), Random.Range(3.0f, 3.5f)), Vector3.up);
     }
 
+    // Remove the farthest back segment
     private void RemoveFirstSegment()
     {
         pieces[0].DeleteSegment(0);
     }
 
+    // Create the first pieces
     private void GenerateInitialPieces()
     {
         pieces[0].AddSegment(new Vector3(0.0f, 0.0f, 3.0f), Vector3.up);
@@ -103,33 +111,36 @@ public class TrackGenerator : MonoBehaviour
             GenerateNextSegment();
     }
 
+    // Add the generated meshes to the world
     public void RoadMeshPieceGenerated(List<Mesh> meshes, bool isBarrier = false)
     {
-        string[] pieceNames = { "Road", "BarrierR", "BarrierL"};
         string[] sides = { "Left", "Top", "Right", "Bottom"};
 
+        // For each mesh
         for (int i = roadMeshChildrenCount; i < meshes.Count + roadMeshChildrenCount; i++)
         {
+            // Is this the left/right piece of a mesh?
             bool isSide = meshes[i - roadMeshChildrenCount].name.Contains("Left") || meshes[i - roadMeshChildrenCount].name.Contains("Right");
 
+            // Create the GameObject
             roadMeshChildren.Add(new GameObject());
             roadMeshChildren[i].transform.parent = transform;
             roadMeshChildren[i].AddComponent<MeshRenderer>().material = isBarrier ? (isSide ? barrierSideMaterial : barrierTopMaterial) : roadMaterial;
-            roadMeshChildren[i].AddComponent<MeshFilter>();
-            roadMeshChildren[i].AddComponent<MeshCollider>();
-            roadMeshChildren[i].GetComponent<MeshFilter>().sharedMesh = meshes[i - roadMeshChildrenCount];
+            roadMeshChildren[i].AddComponent<MeshFilter>().sharedMesh = meshes[i - roadMeshChildrenCount];
             roadMeshChildren[i].AddComponent<MeshCollider>().sharedMesh = meshes[i - roadMeshChildrenCount];
 
+            // Name the mesh
             roadMeshChildren[i].name = "Seg" + pieces[0].totalSegmentTracker + "/";
-
             if (isBarrier)
                 roadMeshChildren[i].name += "Barrier/" + sides[(i - roadMeshChildrenCount) % 4];
             else
                 roadMeshChildren[i].name += "Road/" + (i == 0 ? "Top" : "Bottom");
         }
+        // Increment the counter
         roadMeshChildrenCount += meshes.Count;
     }
 
+    // Remove the segment from the world
     public void RoadSegmentDeleted(int segmentIndex)
     {
         for (int i = segmentIndex * 12; i < ((segmentIndex + 1) * 12); i++)
@@ -140,6 +151,7 @@ public class TrackGenerator : MonoBehaviour
         roadMeshChildren.RemoveRange(segmentIndex * 12, 12);
     }
 
+    // Add the mountain mesh to the world
     public void MountainMeshPieceGenerated(Mesh mesh)
     {
         // Add the mountain mesh to the game world
@@ -161,8 +173,10 @@ public class TrackGenerator : MonoBehaviour
         bounds.playerCollideExit += PlayerExitSegment;
     }
 
+    // Remove the mesh from the world
     public void MountainSegmentDeleted(int segmentIndex)
     {
+        // Get the bounds
         SegmentBounds bounds = mountainMeshChildren[segmentIndex].transform.GetComponentInChildren<SegmentBounds>();
 
         // Clear events to clean up memory
@@ -175,6 +189,7 @@ public class TrackGenerator : MonoBehaviour
         mountainMeshChildren.RemoveAt(segmentIndex);
     }
 
+    // Player has entered the bounds of a segment
     public void PlayerEnterSegment(int segmentIndex)
     {
         if (segmentIndex + renderDistance > pieces[0].totalSegmentTracker)
@@ -184,6 +199,7 @@ public class TrackGenerator : MonoBehaviour
         }
     }
 
+    // Player has exited the bounds of a segment
     public void PlayerExitSegment(int segmentIndex)
     {
         if (segmentIndex - renderDistance > pieces[0].totalSegmentTracker - pieces[0].NumSegments)
