d68112b06d9ecdd8caa4eb137636b875c197e97b
PxlHeart <rianyoungni@outlook.com>
2025-12-07 18:19:12 +0000

    Object spawning
    Added powerup spawning
    Made coins spawn in waves rather than continuous

--- Assets/Scripts/TrackSystem/ObjectGenerator.cs
+++ Assets/Scripts/TrackSystem/ObjectGenerator.cs
@@ -4,7 +4,12 @@ using System.Collections.Generic;
 public class ObjectGenerator : MonoBehaviour
 {
     Dictionary<int, PoolSegment> allCurvePoints = new();
-    private float objectYOffset = 2.0f;
+    private float objectYOffset = 2.5f;
+    private float objectXOffsetRange = 35.0f;
+
+    private float initialCoinPlaceChance = 0.9f;
+    private float coinSwitchPlacingChance = 0.01f;
+    private float powerUpPlaceChance = 0.02f;
 
     [SerializeField] private GameObject coinPrefab;
     [SerializeField] private GameObject powerupPrefab;
@@ -55,14 +60,41 @@ public class ObjectGenerator : MonoBehaviour
     public void GenerateObjects(int segmentIndex)
     {
         PoolSegment segment = allCurvePoints[segmentIndex];
+        bool isPlacingCoins = Random.value < initialCoinPlaceChance;
+        float xOffset = Random.Range(-objectXOffsetRange, objectXOffsetRange);
         foreach(Point point in segment.curvePoints)
         {
-            GameObject coin = coinPool.GetObjectFromPool();
-            coin.transform.parent = transform;
-            coin.transform.position = point.pos + point.upDir * objectYOffset;
-            IPoolable coinPoolable = coin.GetComponent<IPoolable>();
-            coinPoolable.SetSegmentIndex(segmentIndex);
-            segment.objects.Add(coinPoolable);
+            if (Random.value < coinSwitchPlacingChance)
+            {
+                xOffset = Random.Range(-objectXOffsetRange, objectXOffsetRange);
+                isPlacingCoins = !isPlacingCoins;
+            }
+            Vector3 placePos = point.pos + point.upDir * objectYOffset + point.sideDir * xOffset;
+
+            if (isPlacingCoins)
+                PlaceCoin(segment, placePos);
+            else if (Random.value < powerUpPlaceChance)
+                PlacePowerup(segment, placePos);
         }
+    }
+
+    private void PlaceCoin(PoolSegment segment, Vector3 pos)
+    {
+        GameObject coin = coinPool.GetObjectFromPool();
+        coin.transform.parent = transform;
+        coin.transform.position = pos;
+        IPoolable coinPoolable = coin.GetComponent<IPoolable>();
+        coinPoolable.SetSegmentIndex(segment.segmentIndex);
+        segment.objects.Add(coinPoolable);
+    }
+
+    private void PlacePowerup(PoolSegment segment, Vector3 pos)
+    {
+        GameObject powerup = powerupPool.GetObjectFromPool();
+        powerup.transform.parent = transform;
+        powerup.transform.position = pos;
+        IPoolable powerupPoolable = powerup.GetComponent<IPoolable>();
+        powerupPoolable.SetSegmentIndex(segment.segmentIndex);
+        segment.objects.Add(powerupPoolable);
     }
 }
